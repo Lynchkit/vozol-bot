493ec7b31baaf7000881a8af62a5c30c2a6de706
unknown <hkey2387@gmail.com>
2025-07-29 03:28:22 +0300

    Picture кнопка

--- bot.py
+++ bot.py
@@ -3312,7 +3312,7 @@ def handle_order_delivered(call: types.CallbackQuery):
     order_id = int(oid)
 
     # 5. Формируем клавиатуру выбора валют
-    currencies = ["cash", "rub", "dollar", "euro", "uah", "iban", "crypto"]
+    currencies = ["cash", "rub", "dollar", "euro", "uah", "iban", "crypto", "free"]
     kb = types.InlineKeyboardMarkup(row_width=3)
     for cur in currencies:
         kb.add(
